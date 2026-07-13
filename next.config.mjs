277955e11094445f7b8b6464cf48ea5d988ad6b9
John9769 <piyan6979@gmail.com>
2026-07-13 21:51:42 +0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,8 @@ import createNextIntlPlugin from 'next-intl/plugin';
 const withNextIntl = createNextIntlPlugin();
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  allowedDevOrigins: ['192.168.0.5'],
+};
 
 export default withNextIntl(nextConfig);
