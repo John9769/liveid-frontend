ffb73266c685836589aebd934248bacd724addef
John9769 <piyan6979@gmail.com>
2026-07-14 22:39:21 +0800

    fixing routing

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,16 @@ import createNextIntlPlugin from 'next-intl/plugin';
 const withNextIntl = createNextIntlPlugin();
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/:handle((?!en|bm|api|_next|admin|invite|favicon.ico).*)',
+        destination: '/en/verify/:handle',
+        permanent: false,
+      },
+    ];
+  },
+};
 
 export default withNextIntl(nextConfig);
