a33800a83932bf6dd12c67b159d8d06d6b02027a
John9769 <piyan6979@gmail.com>
2026-07-17 17:04:07 +0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -7,7 +7,7 @@ const nextConfig = {
   async redirects() {
     return [
       {
-        source: '/:handle((?!en|bm|admin|invite|terms|privacy|api|_next|favicon|register|login|vault|waitlist|payment|reset-password|verify)[a-z0-9_]+)',
+        source: '/:handle((?!en|ms|admin|invite|terms|privacy|api|_next|favicon|register|login|vault|waitlist|payment|reset-password|verify)[a-z0-9_]+)',
         destination: '/en/verify/:handle',
         permanent: false,
       },
